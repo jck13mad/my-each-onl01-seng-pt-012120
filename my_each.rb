d3f872543136dbac5a9568c16ab4df568af8a6ce
Jack Madden <jck13mad@gmail.com>
2020-01-31 03:04:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,7 +3,8 @@ def my_each(array) # put argument(s) here
   i = 0
   
   while i < array.length
-    yield (array[i])
-    i = i + 1
+    collection << yield(array[i])
+    i += 1
   end
+  collection
 end
